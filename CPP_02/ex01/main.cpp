e5c9e830b37e8216a3284c0c31724bb5f000125c
Urbano Jr <urbanobazz@gmail.com>
2024-05-24 10:21:22 +0200

    tiny refactor

--- CPP_02/ex01/main.cpp
+++ CPP_02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 10:29:50 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/05/23 14:53:40 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/05/23 20:42:33 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@
 
 int main( void ) {
 	Fixed a;
-	Fixed const b( 10 ); Fixed const c( 42.42f ); Fixed const d( b );
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+
 	a = Fixed( 1234.4321f );
+
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
 	return 0;
 }
